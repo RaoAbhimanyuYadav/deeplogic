1c8630aab2f131ba404778b0918a1a894c5a7b9c
Abhimanyu Yadav <raoyadavabhi@gmail.com>
2022-12-22 14:34:07 +0530

    intialized

--- pdfExtractor/models.py
+++ pdfExtractor/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Text(models.Model):
+    filename = models.CharField(max_length=100)
+    des = models.TextField()
+
+    def __str__(self):
+        return self.filename
