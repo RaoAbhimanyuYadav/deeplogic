a53c30943a170b605d82601258d45215eeaee863
Abhimanyu Yadav <raoyadavabhi@gmail.com>
2022-12-22 21:03:12 +0530

    png and jpeg added

--- pdfExtractor/views.py
+++ pdfExtractor/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# from PyPDF2 import PdfReader
+import platform
+from tempfile import TemporaryDirectory
+from pathlib import Path
+
+import pytesseract
+from pdf2image import convert_from_path
+from PIL import Image
 
 from .forms import UploadFileForm
 from .models import Text
-
-# reader = PdfReader("example.pdf")
-# page = reader.pages[0]
-# print(page.extract_text())
 
 
 def index(request):
@@ -17,17 +18,48 @@ def index(request):
 
 
 def upload_file(request):
+    message = ""
     if request.method == "POST":
         form = UploadFileForm(request.POST, request.FILES)
         file = request.FILES["file"]
-        # file = request.FILES.getlist('file')[1]
-        # reader = PdfReader(file)
-        # page = reader.pages[0]
-        # print(page.extract_text())
-        # pdf = PDF.objects.create(file=file)
-        # pdf.save()
-        return HttpResponse(str(file))
+
+        if file.content_type == 'application/pdf':
+            inst = Text.objects.create(filename=str(file), file=file)
+            inst.save()
+
+            if platform.system() == "Windows":
+                path_to_poppler_exe = Path(r"C:\.....")
+                pytesseract.pytesseract.tesseract_cmd = (
+                    r"C:\ProgramFiles\Tesseract-OCR\tesseract.exe")
+
+            PDF_file = Path(f"media/pdf/{inst.filename}")
+            image_file_list = []
+
+            with TemporaryDirectory() as tempdir:
+                if platform.system() == "Windows":
+                    pdf_pages = convert_from_path(PDF_file, 500, poppler_path=path_to_poppler_exe)
+                else:
+                    pdf_pages = convert_from_path(PDF_file, 500)
+                for page_enumeration, page in enumerate(pdf_pages, start=1):
+                    filename = f"{tempdir}\page_{page_enumeration:03}.jpg"
+                    page.save(filename, "JPEG")
+                    image_file_list.append(filename)
+                ocr_text = ""
+                for image_file in image_file_list:
+                    text = str(((pytesseract.image_to_string(Image.open(image_file)))))
+                    ocr_text += text
+                inst.des = ocr_text
+                inst.save()
+            message = f'{inst.filename} uploaded succesfully'
+        elif file.content_type == 'image/jpeg' or 'image/png':
+            text = str(((pytesseract.image_to_string(Image.open(file)))))
+            text = text.replace(r"-\n", "<br/>")
+            inst = Text.objects.create(filename=str(file), file=file, des=text)
+            inst.save()
+            message = f"{inst.filename} uploaded successfully"
+        else:
+            message = "Please upload JPEG or PNG or PDF file only"
     else:
         form = UploadFileForm()
-    context = {"form": form}
+    context = {"form": form, "message": message}
     return render(request, "pdfExtractor/upload.html", context)
