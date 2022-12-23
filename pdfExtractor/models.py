f1aeb232fe707d0d457224d918eb6bb39627fe99
Abhimanyu Yadav <raoyadavabhi@gmail.com>
2022-12-23 11:09:17 +0530

    owner field added in Text model

--- pdfExtractor/models.py
+++ pdfExtractor/models.py
@@ -1,9 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
 class Text(models.Model):
+    owner = models.ForeignKey(
+        User, blank=True, null=True, on_delete=models.CASCADE)
     filename = models.CharField(max_length=100)
     des = models.TextField(null=True, default=None)
     file = models.FileField(upload_to='pdf/', null=True, default=None)
