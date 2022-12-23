0f4f094ab31b2c7c061ff4efe0dfd35cdb47eea6
Abhimanyu Yadav <raoyadavabhi@gmail.com>
2022-12-23 12:04:11 +0530

    comments added for OCR function

--- pdfExtractor/views.py
+++ pdfExtractor/views.py
@@ -34,18 +34,19 @@ def upload_file(request):
     if request.method == "POST":
         form = UploadFileForm(request.POST, request.FILES)
         file = request.FILES["file"]
-
+        # if file type PDF then create instance of text without description
         if file.content_type == 'application/pdf':
             inst = Text.objects.create(filename=str(file), file=file, owner=user)
             inst.save()
-
+            # if platform is window give the location of pytesseract
             if platform.system() == "Windows":
                 path_to_poppler_exe = Path(r"C:\.....")
                 pytesseract.pytesseract.tesseract_cmd = (
                     r"C:\ProgramFiles\Tesseract-OCR\tesseract.exe")
+            # now location of uploaded file is given as PDF_file & image list all pages of PDF as image
             PDF_file = Path(f"media/{inst.file.name}")
             image_file_list = []
-
+            # a temporary loaction is make to convert pdf pages to images
             with TemporaryDirectory() as tempdir:
                 if platform.system() == "Windows":
                     pdf_pages = convert_from_path(PDF_file, 500, poppler_path=path_to_poppler_exe)
@@ -55,14 +56,17 @@ def upload_file(request):
                     filename = f"{tempdir}\page_{page_enumeration:03}.jpg"
                     page.save(filename, "JPEG")
                     image_file_list.append(filename)
+                # ocr text will be hold text of the images and join them
                 ocr_text = ""
                 for image_file in image_file_list:
+                    # pytesseract will extract the text
                     text = str(((pytesseract.image_to_string(Image.open(image_file)))))
                     ocr_text += text
                 inst.des = ocr_text
                 inst.save()
             message = f'{inst.filename} uploaded succesfully'
         elif file.content_type == 'image/jpeg' or 'image/png':
+            # for a single image directly text can be extracted and stored 
             text = str(((pytesseract.image_to_string(Image.open(file)))))
             inst = Text.objects.create(filename=str(file), file=file, des=text, owner=user)
             inst.save()
